a3cc88ae4c5688e8452c665dc2ef73edb7bf8c89
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-01-21 19:17:01 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 #[cfg(not(target_arch = "wasm32"))]
 use workflow_http::error::Error;
 
-//#[cfg(not(target_arch = "wasm32"))]
-//mod builder;
-
-// #[cfg(not(target_arch = "wasm32"))]
-// mod tokens;
-
 #[cfg(not(target_arch = "wasm32"))]
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     use std::collections::HashMap;
-    //use std::io::Error;
-    //use kaizen::result::Result;
     use workflow_http::{BasicAuthenticator, stores, Router};
-    //use flow_server::error::*;
-    use std::{fs, io, collections::BTreeMap};
+    use std::collections::BTreeMap;
     use std::path::Path;
     use duct::cmd;
 
@@ -26,17 +17,17 @@ async fn main() -> Result<(), Error> {
         cmd!("npm","install").run()?;
     }
 
-    if Path::new("./root/workflow").exists() != true {
-        println!("\n\nworkflow wasm folder is absent... running wasm-pack...\n");
+    if Path::new("./root/kaspa").exists() != true {
+        println!("\n\nkaspa wasm folder is absent... running wasm-pack...\n");
         cmd!(
             "wasm-pack",
             "build",
             "--target",
             "web",
             "--out-name",
-            "workflow",
+            "kaspa",
             "--out-dir",
-            "../root/workflow",
+            "../root/kaspa",
             "--features","test"
         )
         .dir("wasm")
@@ -59,98 +50,7 @@ async fn main() -> Result<(), Error> {
     source_map.insert("workflow", "/root/workflow");
     //source_map.insert("solflare-wallet-web", "/node_modules/@solflare-wallet/sdk/lib/esm/adapters/");
     source_map.insert("node_modules", "/node_modules");
-    let mut overrides = BTreeMap::from([
-        (
-            "eventemitter3".to_string(),
-            //"/node_modules/eventemitter3/umd/eventemitter3.js".to_string()
-            "/lib/esm/eventemitter3.js".to_string(),
-        ),
-        (
-            "@solana/web3.js".to_string(),
-            //"/node_modules/@solana/web3.js/lib/index.iife.js".to_string(),
-            "/lib/esm/solana.js".to_string(),
-        ),
-        (
-            "@solflare-wallet/sdk".to_string(),
-            "/lib/esm/solflare-sdk.js".to_string()
-        ),
-        /*(
-            "@solflare-wallet/sdk".to_string(),
-            "/node_modules/@solflare-wallet/sdk/lib/esm/index.js".to_string()
-        ),
-        (
-            "./adapters/web".to_string(),
-            "./adapters/web.js".to_string()
-        ),
-        (
-            "./adapters/iframe".to_string(),
-            "./adapters/iframe.js".to_string()
-        ),
-        (
-            "./base".to_string(),
-            "./base.js".to_string()
-        ),
-        (
-            "bs58".to_string(),
-            "/lib/esm/bs58.js".to_string()
-        ),
-        (
-            "uuid".to_string(),
-            "/node_modules/uuid/wrapper.mjs".to_string()
-        ),
-        (
-            "@project-serum/sol-wallet-adapter".to_string(),
-            "/node_modules/@project-serum/sol-wallet-adapter/dist/esm/index.js".to_string()
-        )*/
-        /*(
-            "@ledgerhq/hw-transport".to_string(),
-            "/node_modules/@ledgerhq/hw-transport/lib-es/Transport.js".to_string()
-        ),
-        (
-            "@ledgerhq/errors".to_string(),
-            "/node_modules/@ledgerhq/errors/lib-es/index.js".to_string()
-        ),
-        (
-            "tweetnacl".to_string(),
-            "/node_modules/tweetnacl/nacl-fast.js".to_string()
-        ),
-        (
-            "@solana/buffer-layout".to_string(),
-            "/node_modules/@solana/buffer-layout/lib/Layout.js".to_string()
-        )*/
-    ]);
-    let adapters = fs::read_dir("./node_modules/@solana")
-        .map_err(|e|{ panic!("\n\nError reading './node_modules/@solana'...\n{}\n\nDid you run npm install?\n\n",e.to_string()); })
-        .unwrap()
-        .map(|res| res.map(|e| e.path()))
-        .collect::<std::result::Result<Vec<_>, io::Error>>()?;
-    //println!("adapters: {:#?}", adapters);
-
-    for adapter in adapters{
-        let path = adapter.as_path();
-        //println!("path: {:?}", path);
-        if let Some(s) = path.to_str(){
-            if !s.contains(&"wallet-adapter-"){
-                continue;
-            }
-            if let Some(file_name) = path.file_name(){
-                if let Some(name) = file_name.to_str(){
-                    //println!("name: {}", &name);
-                    if name.eq("wallet-adapter-solflare"){
-                        overrides.insert(
-                            format!("@solana/{}", name),
-                            format!("/lib/esm/solflare.js"),
-                        );
-                    }else{
-                        overrides.insert(
-                            format!("@solana/{}", name),
-                            format!("/node_modules/@solana/{}/lib/esm/index.js", name),
-                        );
-                    }
-                }
-            }
-        }
-    }
+    let overrides = BTreeMap::from([]);
 
     let router = Router::new_with_overrides(
         cwd.clone(),
