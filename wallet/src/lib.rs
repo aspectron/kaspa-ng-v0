61696afea1db058be267cc855b2ce8394746f752
Surinder Singh Matoo <surinder83singh@gmail.com>
2023-01-28 08:39:09 +0530

    cleanup

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,5 +1,5 @@
 pub use addresses::{Address, AddressError, Prefix as AddressPrefix};
-use kaspa_bip32::*;
+pub use kaspa_bip32::*;
 pub use std::sync::Arc;
 pub use wasm_bindgen::prelude::*;
 pub use workflow_core::task::yield_executor;
@@ -7,23 +7,14 @@ pub use workflow_log::log_trace;
 
 mod manager;
 mod storage;
-mod wallets;
-mod wrapper;
+
+pub use kaspa_wallet_core::{dummy_address, error::Error, wallets::*, Result, WalletWrapper};
+pub use manager::WalletManager;
+pub use storage::WalletStore;
 
 pub enum WalletGeneration {
     Gen0,
     Gen1,
-}
-pub use manager::WalletManager;
-pub use storage::WalletStore;
-pub use wallets::*;
-pub use wrapper::WalletWrapper;
-pub fn dummy_address() -> Address {
-    Address {
-        prefix: AddressPrefix::Mainnet,
-        payload: vec![0u8; 32],
-        version: 0u8,
-    }
 }
 
 #[cfg(any(test, feature = "test"))]
--- wallet/src/wallets/mod.rs
+++
@@ -1,5 +0,0 @@
-mod gen0;
-mod gen1;
-
-pub use gen0::*;
-pub use gen1::*;
