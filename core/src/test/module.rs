1c926ca507cec2b70dd0374e12086da81a153042
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-08-01 18:44:07 +0300

    remove test module (due to removal of timers from workflow-wasm

--- core/src/test/module.rs
+++ core/src/test/module.rs
@@ -1,11 +1,11 @@
-use crate::application::Event;
+// use crate::application::Event;
 use crate::prelude::*;
 use workflow_ux::result::Result;
 
 pub struct Menu {
     pub root: SectionMenu,
     pub group: MenuGroup,
-    pub test: MenuItem,
+    // pub test: MenuItem,
 }
 
 impl Menu {
@@ -16,51 +16,52 @@ impl Menu {
             Icon::Wip
         );
         let group = menu_group!(root, "Test");
-        let test = menu_item!(group, "Form", Icon::Wip, Test::test_form);
-        Ok(Self { root, group, test })
+        // let test = menu_item!(group, "Form", Icon::Wip, Test::test_form);
+        // Ok(Self { root, group, test })
+        Ok(Self { root, group })
     }
 }
 
-#[derive(Module)]
-pub struct Test {
-    pub menu: Menu,
-    _handle: workflow_wasm::timers::IntervalHandle,
-}
+// #[derive(Module)]
+// pub struct Test {
+//     pub menu: Menu,
+//     _handle: workflow_wasm::timers::IntervalHandle,
+// }
 
-impl Test {
-    pub fn new() -> Result<Self> {
-        //let mut count = 0;
-        let cl = Closure::<dyn FnMut()>::new(move || {
-            //log_trace!("set_interval");
-            //count += 1;
-            //if count == 20{
-            //    application().reflect(Event::Halt);
-            //workflow_wasm::timers::clear_interval(_handle);
-            //}else if count < 20{
-            application().reflect(Event::Balance(Id::new()));
+// impl Test {
+//     pub fn new() -> Result<Self> {
+//         //let mut count = 0;
+//         let cl = Closure::<dyn FnMut()>::new(move || {
+//             //log_trace!("set_interval");
+//             //count += 1;
+//             //if count == 20{
+//             //    application().reflect(Event::Halt);
+//             //workflow_wasm::timers::clear_interval(_handle);
+//             //}else if count < 20{
+//             application().reflect(Event::Balance(Id::new()));
 
-            application().reflect(Event::Blocks(Id::new().to_bytes()[0] as u32));
-            //}
-        });
-        let _handle = workflow_wasm::timers::set_interval(&cl, 500)?;
-        log_trace!("set_interval handle: {:?}", _handle);
-        cl.forget();
+//             application().reflect(Event::Blocks(Id::new().to_bytes()[0] as u32));
+//             //}
+//         });
+//         let _handle = workflow_wasm::timers::set_interval(&cl, 500)?;
+//         log_trace!("set_interval handle: {:?}", _handle);
+//         cl.forget();
 
-        Ok(Self {
-            menu: Menu::new()?,
-            _handle,
-        })
-    }
-}
+//         Ok(Self {
+//             menu: Menu::new()?,
+//             _handle,
+//         })
+//     }
+// }
 
-#[async_trait_without_send]
-impl ModuleInterface for Test {
-    async fn main(self: Arc<Self>) -> Result<()> {
-        log_trace!("test:main");
+// #[async_trait_without_send]
+// impl ModuleInterface for Test {
+//     async fn main(self: Arc<Self>) -> Result<()> {
+//         log_trace!("test:main");
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
 
 #[form(title = "Test Form")]
 struct TestForm {
@@ -82,14 +83,14 @@ impl FormHandler for TestForm {
     }
 }
 
-impl Test {
-    async fn test_form(self: Arc<Self>) -> Result<()> {
-        let main = workspace().main();
-        main.swap_from().await?;
+// impl Test {
+//     async fn test_form(self: Arc<Self>) -> Result<()> {
+//         let main = workspace().main();
+//         main.swap_from().await?;
 
-        let view = TestForm::try_create_layout_view(Some(self.clone())).await?;
+//         let view = TestForm::try_create_layout_view(Some(self.clone())).await?;
 
-        main.swap_to(view).await?;
-        Ok(())
-    }
-}
+//         main.swap_to(view).await?;
+//         Ok(())
+//     }
+// }
